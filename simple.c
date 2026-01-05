ac7299e8368beb787535579b349bfb1cbe7f16a6
Felix Lauwerie <flauweri@z4r10p2.42lyon.fr>
2026-01-05 17:08:53 +0100

    simple

--- simple.c
+++ simple.c
@@ -3,71 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   simple.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mobenhab <mobenhab@student.42.fr>          +#+  +:+       +#+        */
+/*   By: flauweri <flauweri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:34:09 by mobenhab          #+#    #+#             */
-/*   Updated: 2025/12/31 20:53:55 by mobenhab         ###   ########.fr       */
+/*   Updated: 2026/01/03 19:26:42 by flauweri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	check_top(t_stack *pile_a, t_stack *pile_b)
+void	check_top(t_stack *pile_a, t_stack *pile_b, int *stack_size)
 {
-
+	pb(pile_b, pile_a);
+	(*stack_size)++;
 }
 
-void	check_last(t_stack *pile_a, t_stack *pile_b)
+void	check_last(t_stack *pile_a, t_stack *pile_b, int *stack_size, t_list **last)
 {
-
+		*last = pile_a->first;
+		pb(pile_b, pile_a);
+		rb(pile_b);
+		(*stack_size)++;
 }
 
-void insertion(t_stack *pile_a, t_stack *pile_b)
+void	check_middle(t_stack *pile_a, t_stack *pile_b, int *stack_size)
 {
-    t_list *last;//connaitre le content du dernier noeud
-	t_list *mouv;//pouvoir se deplacer dans la liste b
-	int		index;//connaitre le nombre d'element dans b pour savoir dans quel sens decaler(permet de diminuer le nombre d'operation)
+	t_list	*mouv;
+	int		rotation;
+
+	mouv = pile_b->first;
+	rotation = 0;
+	while (mouv->next != NULL)
+	{
+		if (pile_a->first->content > mouv->content)
+		{
+			if (mouv->index <= (*stack_size / 2))
+			{
+				while (rotation++ <= mouv->index)
+					rb(pile_b);
+				pb(pile_b, pile_a);
+				while (rotation-- > 0)
+					rrb(pile_b);
+			}
+			else
+			{
+				while (rotation++ <= mouv->index)
+					rrb(pile_b);
+				pb(pile_b, pile_a);
+				while (rotation-- > 0)
+					rb(pile_b);
+			}
+			(*stack_size)++;
+			return ;
+		}
+		mouv = mouv->next;
+	}
+}
+
+void	ft_insertion(t_stack *pile_a, t_stack *pile_b)
+{
+    t_list *last;
 	int		stack_size;
 
 	if (!pile_a || !pile_a->first || !pile_a->first->next)
     	return ;
-	pb(pile_a, pile_b);//mettre un premier element dans b
-	last = pile_b->first;// le definir comme le dernier pour avoir un repère
-	index = 1;//index qui va permettre de savoir dans quel sens rotate pour empiler
-	while (pile_a->first)//parcours la chaine a jusqu'à que tout soit trier puis mettre tout b dans a
+	pb(pile_b, pile_a);
+	last = pile_b->first;
+	stack_size = 1;
+	while (pile_a->first)
 	{
-		if (pile_a->first->content > pile_b->first->content)//si top(a) > top(b), on empile
-		{
-			last = pile_b->first;
-			pb(pile_b, pile_a);//mettre au dessus de b
-			stack_size++;
-		}
+		if (pile_a->first->content > pile_b->first->content)
+			check_top(pile_a, pile_b, &stack_size);
 		else if (pile_a->first->content < last->content)
-		{
-			last = pile_a->first;
-			pb(pile_b, pile_a);//mettre au dessus de b
-			rb(pile_b);//passer le top en bas de la pile
-			stack_size++;
-		}
+			check_last(pile_a, pile_b, &stack_size, &last);
 		else
-		{
-			mouv = pile_b->first;//premier element de la liste b
-			while (mouv->next != NULL)
-			{
-				if (pile_a->first->content > mouv->content)
-				{
-					if (index <= (stack_size/2))//savoir si on se situe a plus de la moitié ou non
-						// while ()
-							rb(pile_b);
-				}
-				mouv = mouv->next;
-				index++;
-			}
-		}	
+			check_middle(pile_a, pile_b, &stack_size);
 	}
+	while (pile_b->first)
+		pa(pile_a, pile_b);
 }
 
-// int main(void)
-// {
-	
-// }
