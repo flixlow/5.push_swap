0f41cb8ced0503bf2781e764ab3f07d0e1df03ff
Felix Lauwerie <flauweri@z1r4p3.42lyon.fr>
2026-01-06 11:46:22 +0100

    need to add index to pile_b in simple

--- simple.c
+++ simple.c
@@ -6,39 +6,50 @@
 /*   By: flauweri <flauweri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:34:09 by mobenhab          #+#    #+#             */
-/*   Updated: 2026/01/06 10:11:19 by flauweri         ###   ########.fr       */
+/*   Updated: 2026/01/06 11:03:43 by flauweri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	check_top(t_stack *pile_a, t_stack *pile_b, int *stack_size)
+void	check_top(t_stack *pile_a, t_stack *pile_b, int *size)
 {
 	pb(pile_b, pile_a);
-	(*stack_size)++;
+	(*size)++;
 }
 
-void	check_last(t_stack *pile_a, t_stack *pile_b, int *stack_size, t_list **last)
+void	check_last(t_stack *pile_a, t_stack *pile_b, int *size, t_list **last)
 {
 		*last = pile_a->first;
 		pb(pile_b, pile_a);
 		rb(pile_b);
-		(*stack_size)++;
+		(*size)++;
 }
 
-void	rotation_count(t_stack *pile_a, t_stack *pile_b, void *f, void *g)
+void	rotation_count(t_stack *pile_a, t_stack *pile_b, t_list *mouv, int *size)
 {
 	int	rotation;
 	
 	rotation = 0;
-	while (rotation++ <= mouv->index)
-		f(pile_b);
-	pb(pile_b, pile_a);
-	while (rotation-- > 0)
-		g(pile_b);
+	if (mouv->index <= (*size / 2))
+	{
+		while (rotation++ <= mouv->index)
+			rb(pile_b);
+		pb(pile_b, pile_a);
+		while (rotation-- >= 0)
+			rrb(pile_b);
+	}
+	else
+	{
+		while (rotation++ <= mouv->index)
+			rrb(pile_b);
+		pb(pile_b, pile_a);
+		while (rotation-- >= 0)
+			rb(pile_b);
+	}
 }
 
-void	check_middle(t_stack *pile_a, t_stack *pile_b, int *stack_size)
+void	check_middle(t_stack *pile_a, t_stack *pile_b, int *size)
 {
 	t_list	*mouv;
 
@@ -47,11 +58,8 @@ void	check_middle(t_stack *pile_a, t_stack *pile_b, int *stack_size)
 	{
 		if (pile_a->first->content > mouv->content)
 		{
-			if (mouv->index <= (*stack_size / 2))
-				rotation_count(pile_a, pile_b, rb, rrb);
-			else
-				rotation_count(pile_a, pile_b, rrb, rb);
-			(*stack_size)++;
+			rotation_count(pile_a, pile_b, mouv, size);
+			(*size)++;
 			return ;
 		}
 		mouv = mouv->next;
@@ -61,21 +69,21 @@ void	check_middle(t_stack *pile_a, t_stack *pile_b, int *stack_size)
 void	ft_insertion(t_stack *pile_a, t_stack *pile_b)
 {
     t_list *last;
-	int		stack_size;
+	int		size;
 
 	if (!pile_a || !pile_a->first || !pile_a->first->next)
     	return ;
 	pb(pile_b, pile_a);
 	last = pile_b->first;
-	stack_size = 1;
+	size = 1;
 	while (pile_a->first)
 	{
 		if (pile_a->first->content > pile_b->first->content)
-			check_top(pile_a, pile_b, &stack_size);
+			check_top(pile_a, pile_b, &size);
 		else if (pile_a->first->content < last->content)
-			check_last(pile_a, pile_b, &stack_size, &last);
+			check_last(pile_a, pile_b, &size, &last);
 		else
-			check_middle(pile_a, pile_b, &stack_size);
+			check_middle(pile_a, pile_b, &size);
 	}
 	while (pile_b->first)
 		pa(pile_a, pile_b);
@@ -91,7 +99,11 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		return (0);
-
+	pile_a = creatpile();
+    pile_b = creatpile();
+    if (!pile_a || !pile_b)
+        return (1);
+	pile_a->first = NULL;	
 	init_stack(pile_a, argv, 1);
 	pile_b->first = NULL;
 
@@ -103,5 +115,7 @@ int	main(int argc, char **argv)
 	printpile(pile_a);
 	printpile(pile_b);
 
+	freepile(pile_a);
+	freepile(pile_b);
 	return (0);
 }
